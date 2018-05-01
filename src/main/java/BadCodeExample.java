96ffd818778f6349bb306637a36f7f732a006857
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2018-05-01 23:43:26 +0300

    first commit

---
+++ src/main/java/BadCodeExample.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+
+public class BadCodeExample {
+
+public static void main(String args[]) throws InterruptedException {
+
+    String searchTerm = "Selenium";
+    System.out.println("Hello World!!!");
+    WebDriver webDriver = new FirefoxDriver();
+    webDriver.get("https://www.google.com");
+    WebElement searchField = webDriver.findElement(By.name("q"));
+    searchField.sendKeys(searchTerm);
+    searchField.sendKeys(Keys.ENTER);
+    sleep (3000);
+    List <WebElement> searchResults = webDriver.findElements(By.xpath("//div[@class='srg']/*[@class='g']"));
+    System.out.println(searchResults.size()+ " search results are displayed on the page");
+    for(WebElement searchResult : searchResults){
+        String searchResultText = searchResult.getText();
+        if(searchResultText.contains(searchTerm)) {
+            System.out.println("Search term "+ searchTerm +" found in result");
+            System.out.println("--------------------------------------------");
+        }
+    }
+
+    //sleep (5000);
+    webDriver.close();
+
+
+}
+
+}
