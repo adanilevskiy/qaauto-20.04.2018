cd7cebb882d60ae11cbfbe72589739cdd3086988
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2018-05-04 19:01:28 +0300

    Updated successfulLoginTest

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -5,20 +5,29 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static java.lang.Thread.sleep;
+
 public class LinkedinLoginTest {
 
     @Test
-    public void successfulLoginTest() {
+    public void successfulLoginTest() throws InterruptedException {
         WebDriver webDriver = new FirefoxDriver();
         webDriver.get("https://www.linkedin.com");
 
-        Assert.assertEquals(webDriver.getTitle(), "Expected", "Page title is wrong");
+        Assert.assertEquals(webDriver.getTitle(), "LinkedIn: Log In or Sign Up", "Page title is wrong");
 
         WebElement userEmailField = webDriver.findElement(By.xpath("//input[@id='login-email']"));
         WebElement userPasswordField = webDriver.findElement(By.xpath("//input[@id='login-password']"));
         WebElement signInButton = webDriver.findElement(By.xpath("//input[@id='login-submit']"));
 
+        userEmailField.sendKeys("toshnot.1@gmail.com");
+        userPasswordField.sendKeys("Test123");
+        signInButton.click();
+        sleep (5000);
 
+        Assert.assertEquals(webDriver.getTitle(), "LinkedIn", "User is not logged in");
+
+        webDriver.close();
     }
 
 }
